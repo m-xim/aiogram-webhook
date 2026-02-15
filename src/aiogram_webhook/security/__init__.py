89528937bb690847198efb4bf1034c18a83ab3d6
m-xim <170838360+m-xim@users.noreply.github.com>
2026-02-15 16:42:13 +0300

    fix(security): Check to SecurityCheck

--- src/aiogram_webhook/security/__init__.py
+++ src/aiogram_webhook/security/__init__.py
@@ -1,6 +1,6 @@
-from aiogram_webhook.security.checks.check import Check
+from aiogram_webhook.security.checks.check import SecurityCheck
 from aiogram_webhook.security.checks.ip import IPCheck
 from aiogram_webhook.security.secret_token import SecretToken, StaticSecretToken
 from aiogram_webhook.security.security import Security
 
-__all__ = ("Check", "IPCheck", "SecretToken", "Security", "StaticSecretToken")
+__all__ = ("IPCheck", "SecretToken", "Security", "SecurityCheck", "StaticSecretToken")
